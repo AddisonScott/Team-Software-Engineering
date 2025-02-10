0044ac2a89543db9a9d2852f006d062421d8dd91
AddisonScott <159310467+AddisonScott@users.noreply.github.com>
2025-02-10 00:05:26 +0000

    Two line limit + erase + distant dependant lines

--- Assets/Scripts/DrawManager.cs
+++ Assets/Scripts/DrawManager.cs
@@ -4,9 +4,10 @@ public class DrawManager : MonoBehaviour
 {
     public Camera _cam;
     [SerializeField] private Line _linePrefab;
-
+    private Line[] _lines = new Line[2]; 
     public const float RESOLUTION = 0.1f;
     private Line _currentLine;
+    private int lineNumber = 0;
     void Start()
     {
         
@@ -15,10 +16,42 @@ public class DrawManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = _cam.ScreenToWorldPoint( Input.mousePosition );
 
-        if (Input.GetMouseButtonDown(0)) _currentLine = Instantiate(_linePrefab);
+        Vector2 mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        if (lineNumber < _lines.Length)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _currentLine = Instantiate(_linePrefab);
+                _lines[lineNumber] = _currentLine;
+                lineNumber++;
+            }
+        }
 
-        if(Input.GetMouseButton(0)) _currentLine.SetPosition( mousePos );
+
+        if (Input.GetMouseButton(0) && _currentLine != null)
+        {
+            _currentLine.SetPosition(mousePos);
+        }
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _currentLine = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = _lines.Length - 1; i >= 0; i--)
+            {
+                if (_lines[i] != null)
+                {
+                    Destroy(_lines[i].gameObject); 
+                    _lines[i] = null; 
+                    lineNumber--; 
+                    break;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using UnityEngine;
-using System.Collections.Generic; 
+using System.Collections.Generic;
+using System;
 
 public class Line : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class Line : MonoBehaviour
     [SerializeField] private EdgeCollider2D _collider;
 
     private List<Vector2> _points = new List<Vector2>();
+   
 
+    public int distanceLimit = 3;
     public void SetPosition(Vector2 pos) { 
         if (!CanAppend(pos)) return;
 
@@ -23,7 +26,24 @@ public class Line : MonoBehaviour
     private bool CanAppend(Vector2 pos)
     {
         if (_renderer.positionCount == 0) return true; //always true for first point
-
+        if (GetTotalDistance() >= distanceLimit) return false; //returns false when line too big
         return Vector2.Distance(_renderer.GetPosition(_renderer.positionCount - 1), pos) > DrawManager.RESOLUTION; //checks distance between last point
+    }
+
+    private float GetTotalDistance()
+    {
+        //Finds total distance between each point
+        float totalDistance = 0f;
+        if (_points.Count > 1) //if list only had one element, you couldn't find the distance
+        {
+            for (int i = 1; i < _points.Count; i++)
+            {
+                totalDistance += Vector2.Distance(_points[i - 1], _points[i]);
+            }
+
+        }
+        return totalDistance;
+
+
     }
 }
