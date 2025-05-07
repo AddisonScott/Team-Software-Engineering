4161ceb7a899e9189c9e7c86c04b44f792253615
CJackW <shakierdevyt@gmail.com>
2025-05-07 14:29:36 +0100

    should now be up to date with main

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Goal : MonoBehaviour
+{
+    public Animator UIanimator;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(endScreenTransition());
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private IEnumerator endScreenTransition()
+    {
+        UIanimator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("EndScreen");
+       
+    }
+}
